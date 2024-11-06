a8ade31bcf23974b89a908a3c3dd7d7ae6c6e1b7
RICHARLE FAGUNDES DO AMARAL OLIVEIRA <richarlepampam@gmail.com>
2024-11-06 15:23:24 -0300

    Enviando arquivos para deploy

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar/navbar'
+
+
+const App = () => {
+   return (
+    <div>
+      <Navbar />
+     </div>
+
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <header className="header">
+
+            <a href="/" className="logo">Logo</a>
+
+            <nav className="navbar">
+                <a href="" className="/">Home</a>
+                <a href="" className="/">Sobre</a>
+                <a href="" className="/">Portfólio</a>
+                <a href="" className="/">Serviços</a>
+                <a href="" className="/">Contatos</a>
+
+            </nav>
+
+            </header>
+    )
+}
+
+
+export default Navbar
